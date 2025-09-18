8bf2633b3dbf880c8798c3a8376102f4c6325803
billy <billy.xiezhimin@gmail.com>
2025-09-19 07:46:17 +0800

    feat: update login landing

--- src/pages/sys/login/index.tsx
+++ src/pages/sys/login/index.tsx
@@ -1,10 +1,11 @@
-import PlaceholderImg from "@/assets/images/background/placeholder.svg";
+import DataPlatformImg from "@/assets/images/background/data-platform-tech.svg";
+import { Icon } from "@/components/icon";
 import LocalePicker from "@/components/locale-picker";
-import Logo from "@/components/logo";
 import { GLOBAL_CONFIG } from "@/global-config";
 import SettingButton from "@/layouts/components/setting-button";
 import { useUserToken } from "@/store/userStore";
 import { Navigate } from "react-router";
+import { useTranslation } from "react-i18next";
 import LoginForm from "./login-form";
 import MobileForm from "./mobile-form";
 import { LoginProvider } from "./providers/login-provider";
@@ -13,6 +14,7 @@ import RegisterForm from "./register-form";
 import ResetForm from "./reset-form";
 
 function LoginPage() {
+	const { t } = useTranslation();
 	const token = useUserToken();
 
 	if (token.accessToken) {
@@ -23,9 +25,9 @@ function LoginPage() {
 		<div className="relative grid min-h-svh lg:grid-cols-2 bg-background">
 			<div className="flex flex-col gap-4 p-6 md:p-10">
 				<div className="flex justify-center gap-2 md:justify-start">
-					<div className="flex items-center gap-2 font-medium cursor-pointer">
-						<Logo size={28} />
-						<span>{GLOBAL_CONFIG.appName}</span>
+					<div className="flex items-center gap-2 text-xl font-semibold text-foreground">
+						<Icon icon="lucide:star" size={28} className="text-primary" />
+						<span>{t("sys.login.brandTitle")}</span>
 					</div>
 				</div>
 				<div className="flex flex-1 items-center justify-center">
@@ -42,7 +44,11 @@ function LoginPage() {
 			</div>
 
 			<div className="relative hidden bg-background-paper lg:block">
-				<img src={PlaceholderImg} alt="placeholder img" className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.5] dark:grayscale" />
+				<img
+					src={DataPlatformImg}
+					alt="technology driven data platform illustration"
+					className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.5] dark:grayscale"
+				/>
 			</div>
 
 			<div className="absolute right-2 top-0 flex flex-row">
